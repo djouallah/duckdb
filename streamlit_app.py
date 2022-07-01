722675a994341edd6e0cafb0c494e07991e5d8ee
Mimoune <djouallah@users.noreply.github.com>
2022-07-01 21:32:52 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,8 @@ st.subheader("Nem  Today: " + now)
 
 
 import altair as alt
-c = alt.Chart(results).mark_area().encode( x='SETTLEMENTDATE:T', y='mwh:Q',color='DUID',tooltip=['SETTLEMENTDATE', 'mwh']).interactive()
+c = alt.Chart(results).mark_area().encode( x='SETTLEMENTDATE:T', y='mwh:Q',color='DUID',
+                                          tooltip=['SETTLEMENTDATE','DUID', 'mwh']).interactive()
 st.write(c)
 #download
 def download_link(object_to_download, download_filename, download_link_text):
