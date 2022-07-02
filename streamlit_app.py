36ae54d406366287b5b2ea830a8da3efe9d25b75
Mimoune <djouallah@users.noreply.github.com>
2022-07-02 10:42:02 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -131,10 +131,15 @@ link='[Data Source](http://nemweb.com.au/Reports/Current/Dispatch_SCADA/)'
 col1.markdown(link,unsafe_allow_html=True)
 
 st.write(files_to_upload)
+@st.cache
+ def convert_df(df):
+     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+     return df.to_csv().encode('utf-8')
 
+csv = convert_df(my_large_df)
 st.download_button(
      label="Download data as CSV",
-     data=df,
+     data=csv,
      file_name='large_df.csv',
      mime='text/csv',
  )
