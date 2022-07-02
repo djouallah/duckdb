257f112aadb9a4f935fb11dbad1edf131036f5bc
Mimoune <djouallah@users.noreply.github.com>
2022-07-02 10:45:02 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -134,7 +134,7 @@ st.write(files_to_upload)
 @st.cache
 def convert_df(df):
      # IMPORTANT: Cache the conversion to prevent computation on every rerun
-     return df.to_csv().encode('utf-8')
+    return df.to_csv().encode('utf-8')
 
 csv = convert_df(my_large_df)
 st.download_button(
